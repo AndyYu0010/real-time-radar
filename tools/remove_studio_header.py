855c6d72b0a8f008f3cf6508447ff6b61933c77b
Andy Yu <andy0010@hotmail.com.tw>
2020-09-10 13:10:41 +0800

    update TI mmWave remove studio header

---
+++ tools/remove_studio_header.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def remove_header(filename, packet_num):
+    """
+    for raw mmWave studio data use
+    :param filename: string
+                    Input file name
+
+    :packet_num: int
+                    Number of Packet
+                    1441: 1 tx
+                    2881: 2 tx
+                    4322: 3 tx
+
+    """
+    bin_data = np.fromfile(filename, dtype=np.int16)
+    index = []
+    for i in range(packet_num):
+        j = i * 735
+        index.append([j, j + 1, j + 2, j + 3, j + 4, j + 5, j + 6])
+    output = np.delete(bin_data, index)
+    return output
+
